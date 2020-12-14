1d3b0507dc0624d10885d01ed00c557aa6861d45
Ines <ines.capela@infolabix.com>
2020-12-14 21:22:52 +0000

    Merged with main

---
+++ app/app/src/main/java/edu/ufp/projectpampaw/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package edu.ufp.projectpampaw.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
---
+++ frontend/TODO.md
@@ -0,0 +1,10 @@
+# Frontend TODO
+This is the markdown todo file for frontend.
+
+## Content
+Tasks related to new content.
+
+- [ ] Example A
+
+# DONE
+- [x] Create Frontend TODO file.
