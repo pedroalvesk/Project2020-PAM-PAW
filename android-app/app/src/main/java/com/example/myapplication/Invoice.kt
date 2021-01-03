1b3b5629f69d3306620dc19093fe96a725f28de3
Ines <ines.capela@infolabix.com>
2021-01-03 20:26:11 +0000

    add Invoice entity

---
+++ android-app/app/src/main/java/com/example/myapplication/Invoice.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.graphics.Bitmap
+import androidx.room.*
+
+@Entity(
+    tableName = "invoice",
+    indices = [Index(value = ["customername"])]
+)
+data class Invoice(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "userID") var userID: Int = 0,
+    @ColumnInfo(name = "filename") var filename: String? = "",
+    @ColumnInfo(name = "extension") var extension: String? = "",
+
+    @Ignore var picture: Bitmap? = null
+)
--- android-app/app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ android-app/app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -1,11 +1,39 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+        val url = "http://192.168.1.150:8081/api/v1/invoices"
+
+        val data = JSONObject()
+        try {
+            //input your API parameters
+
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, data,
+            { response ->
+
+            },
+            { error ->
+                // TODO: Handle error
+            }
+        )
+        val requestQueue = Volley.newRequestQueue(this)
+        requestQueue.add(jsonObjectRequest)
         setContentView(R.layout.activity_main2)
     }
 }
