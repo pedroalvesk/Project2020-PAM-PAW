07a007184d7ceb9c14b862feb4621e7fad967e28
Pedro Alves <pedroalvesk@protonmail.com>
2021-01-03 02:43:25 +0000

    Added frontend

---
+++ frontend/src/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <UsersPage/>
+  </div>
+</template>
+
+<script>
+import UsersPage from "@/components/UsersPage";
+
+export default {
+  name: 'App',
+  components: {
+    UsersPage,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/src/api.js
@@ -0,0 +1,9 @@
+const URL_LOGIN = "http://localhost:8090/api/v1/auth/login"
+const URL_USERS = "http://localhost:8090/api/v1/backoffice/users"
+const URL_INVOICES = "http://localhost:8090/api/v1/backoffice/invoices"
+
+export default {
+    LOGIN: URL_LOGIN,
+    USERS: URL_USERS,
+    INVOICES: URL_INVOICES
+}
