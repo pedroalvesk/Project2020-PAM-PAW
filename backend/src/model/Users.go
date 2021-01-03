427ba5ec81571cc66877435d3c8bddf98db71464
Pedro Alves <pedroalvesk@protonmail.com>
2021-01-03 19:28:51 +0000

    Removed backend

--- backend/src/model/Users.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "github.com/jinzhu/gorm"
-
-type Users struct {
-	gorm.Model `swaggerignore:"true"`
-	Username   string `json:"username"`
-	Password   string `json:"password"`
-}
